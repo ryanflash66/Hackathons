a9fcbfe3d6cfe09cbbf719f1b2d2515f9610f647
Ryan Balungeli <ryanflash166@gmail.com>
2025-06-10 15:19:06 -0400

    Initial Commit - Halloumi Boxes Problem

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int main() {
+    // Your code here
+    return 0;
+}
+
+bool can_sort_array(int n, int k, int arr[]){
+    
+    return true;
+}
